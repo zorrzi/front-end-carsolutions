bd4b16657ab7764a599c2a39c0f868fa6dfbb83f
Mateus Marinheiro <mateusmarinheiro2005@gmail.com>
2024-10-25 00:04:36 -0300

    Estilizando Agendas

--- notes-frontend/src/components/AgendaCliente/index.jsx
+++ notes-frontend/src/components/AgendaCliente/index.jsx
@@ -21,38 +21,69 @@ export default function AgendamentoCliente() {
     });
   }, []);
 
-  return (
-    <div>
-      <h1>Meus Agendamentos</h1>
-      {agendamentos.length === 0 ? (
-        <p>Você não tem agendamentos.</p>
-      ) : (
-        <ul>
-          {agendamentos.map((agendamento, index) => (
-            <li key={index}>
-              <p>Carro: {agendamento.carro}</p>
-              <p>Tipo: {agendamento.tipo}</p>
-              
-              {/* Exibe informações de acordo com o tipo de agendamento */}
-              {agendamento.tipo === 'venda' ? (
-                <>
-                  <p>Data: {agendamento.data}</p>
-                  <p>Horário: {agendamento.horario}</p>
-                </>
-              ) : (
-                <>
-                  <p>Data de Retirada: {agendamento.data_retirada}</p>
-                  <p>Horário de Retirada: {agendamento.horario_retirada}</p>
-                  <p>Data de Devolução: {agendamento.data_devolucao}</p>
-                  <p>Horário de Devolução: {agendamento.horario_devolucao}</p>
-                </>
-              )}
+  // Função para retornar a classe CSS com base no status do agendamento
+  const getStatusClass = (status) => {
+    switch (status.toLowerCase()) {
+      case 'cancelado':
+        return 'status-cancelado';
+      case 'pendente':
+        return 'status-pendente';
+      case 'em atendimento':
+        return 'status-atendimento';
+      case 'concluido':
+        return 'status-concluido';
+      default:
+        return '';
+    }
+  };
 
-              <p>Status: {agendamento.status}</p>
-            </li>
-          ))}
-        </ul>
-      )}
+  // Função para transformar a data no formato dd/mm/aaaa
+  const formatarData = (data) => {
+    const dataSplit = data.split('-');
+    return `${dataSplit[2]}/${dataSplit[1]}/${dataSplit[0]}`;
+  };
+
+  // Trocar a primeira letra para maiúscula
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  return (
+    <div className="lista-agendamentos-container">
+        <h2 className="titulo-agendamentos">Lista de Agendamentos</h2>
+        <table className="tabela-agendamentos">
+            <thead>
+                <tr>
+                    <th>Carro</th>
+                    <th>Operação</th>
+                    <th>Data da Retirada</th>
+                    <th>Horário da Retirada</th>
+                    <th>Data da Devolução</th>
+                    <th>Horário da Devolução</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {agendamentos.length > 0 ? (
+                    agendamentos.map((agendamento, index) => (
+                        <tr key={index}>
+                            <td>{agendamento.carro}</td>
+                            <td>{capitalizeFirstLetter(agendamento.tipo)}</td>
+                            <td>{agendamento.tipo === 'venda' ? formatarData(agendamento.data) : formatarData(agendamento.data_retirada)}</td>
+                            <td>{agendamento.tipo === 'venda' ? agendamento.horario : agendamento.horario_retirada}</td>
+                            <td>{agendamento.tipo === 'venda' ? '' : formatarData(agendamento.data_devolucao)}</td>
+                            <td>{agendamento.tipo === 'venda' ? '' : agendamento.horario_devolucao}</td>
+                            {/* Aplicando a classe condicionalmente com base no status */}
+                            <td className={getStatusClass(agendamento.status)}>{capitalizeFirstLetter(agendamento.status)}</td>
+                        </tr>
+                    ))
+                ) : (
+                    <tr>
+                        <td colSpan="7">Nenhum agendamento encontrado</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
     </div>
   );
 }
