8914ddc0e0cbcd4dc82a61af7f7eeeaca594cb28
Mateus Marinheiro <mateusmarinheiro2005@gmail.com>
2024-10-25 11:10:39 -0300

    Estilizando Atendimento e criando botão de chat

--- notes-frontend/src/components/ListaAtendimentoFuncionario/index.jsx
+++ notes-frontend/src/components/ListaAtendimentoFuncionario/index.jsx
@@ -21,23 +21,44 @@ export default function ListaAtendimentoFuncionario() {
     });
   }, []);
 
+  // Função para formatar a data no formato dd/mm/aaaa
+  function formatarData(data) {
+    const dataSplit = data.split('-');
+    return `${dataSplit[2]}/${dataSplit[1]}/${dataSplit[0]}`;
+  }
+
+  // Função para deixar a primeira letra da string em maiúscula
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+
   return (
-    <div>
-      <h1>Meus Atendimentos</h1>
+    <div className="atendimentos-container">
+      <h1 className="titulo">Lista de atendimento</h1>
       {meusAtendimentos.length === 0 ? (
         <p>Você não tem agendamentos assumidos no momento.</p>
       ) : (
-        <ul>
-          {meusAtendimentos.map((atendimento, index) => (
-            <li key={index}>
-              <p>Carro: {atendimento.carro}</p>
-              <p>Data: {atendimento.data || atendimento.data_retirada}</p>
-              <p>Horário: {atendimento.horario || atendimento.horario_retirada}</p>
-              <p>Tipo: {atendimento.tipo}</p>
-              <p>Status: {atendimento.status}</p>
-            </li>
-          ))}
-        </ul>
+        <table className="atendimentos-tabela">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Operação</th>
+              <th>Data</th>
+              <th>Horário</th>
+            </tr>
+          </thead>
+          <tbody>
+            {meusAtendimentos.map((atendimento, index) => (
+              <tr key={index}>
+                <td>{atendimento.nome}</td>
+                <td>{capitalizeFirstLetter(atendimento.tipo)}</td>
+                <td>{formatarData(atendimento.data) || formatarData(atendimento.data_retirada)}</td>
+                <td>{atendimento.horario || atendimento.horario_retirada}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
