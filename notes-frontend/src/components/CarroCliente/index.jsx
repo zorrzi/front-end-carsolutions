f7aa5401d3b6b79bf6dd679affa1f91f42ae5d64
zorrzi <eduardovz@al.insper.edu.br>
2024-10-29 17:00:28 -0300

    arrumando imagem dos carros

--- notes-frontend/src/components/CarroCliente/index.jsx
+++ notes-frontend/src/components/CarroCliente/index.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import './index.css';
 
 export default function CarroCliente({ car }) {
-  const navigate = useNavigate();  // Hook para navegação
+  const navigate = useNavigate();
 
   const handleVerMais = () => {
-    navigate(`/anuncio/${car.id}`);  // Redireciona para a página do anúncio com o ID do carro
+    navigate(`/anuncio/${car.id}`);
   };
 
   return (
@@ -15,15 +15,13 @@ export default function CarroCliente({ car }) {
         <p className="card-title">{car.brand}</p>
         <p className="card-subtitle">{car.model}</p>
       </div>
-      <img className="carro" src={car.image_url || '/default-image.jpg'} alt={car.model} />
+      <img className="carro" src={car.image_url_1 || '/default-image.jpg'} alt={car.model} />
 
-      {/* Seção de banner para Aluguel e Venda */}
       <div className="banner-container">
         {car.is_for_rent && <span className="banner-item">Aluguel</span>}
         {car.is_for_sale && <span className="banner-item">Venda</span>}
       </div>
 
-      {/* Botão de Ver Mais */}
       <p className="card-content" onClick={handleVerMais}>Ver mais</p>
     </div>
   );
--- notes-frontend/src/components/CatalogoCarrosCliente/index.jsx
+++ notes-frontend/src/components/CatalogoCarrosCliente/index.jsx
@@ -4,7 +4,6 @@ import CarroCliente from '../CarroCliente';
 import './index.css';
 
 export default function CatalogoCarrosCliente() {
-  // Carregar estado inicial do localStorage, se disponível
   const initialFilters = {
     isForSale: localStorage.getItem('isForSale') === 'false' ? false : true,
     isForRent: localStorage.getItem('isForRent') === 'false' ? false : true,
@@ -21,14 +20,12 @@ export default function CatalogoCarrosCliente() {
 
   const [cars, setCars] = useState([]);
   const [filters, setFilters] = useState(initialFilters);
-  const [appliedFilters, setAppliedFilters] = useState(initialFilters); // Armazena filtros aplicados
+  const [appliedFilters, setAppliedFilters] = useState(initialFilters);
 
-  // Salvar mudanças no localStorage e atualizar o estado de filtros
   const handleFilterChange = (e) => {
     const { name, value, type, checked } = e.target;
     const newValue = type === 'checkbox' ? checked : value;
-    
-    // Atualizar localStorage para isForSale e isForRent
+
     if (name === 'isForSale' || name === 'isForRent') {
       localStorage.setItem(name, newValue);
     }
@@ -39,13 +36,10 @@ export default function CatalogoCarrosCliente() {
     }));
   };
 
-  // Função para buscar carros com base nos filtros aplicados
   const fetchCars = useCallback(() => {
     const query = Object.keys(appliedFilters)
       .filter((key) => {
-        // Ignora os filtros de preço de aluguel se a checkbox de aluguel não estiver marcada
         if (!appliedFilters.isForRent && (key === 'minRentPrice' || key === 'maxRentPrice')) return false;
-        // Ignora os filtros de preço de venda se a checkbox de venda não estiver marcada
         if (!appliedFilters.isForSale && (key === 'minSalePrice' || key === 'maxSalePrice')) return false;
         return true;
       })
@@ -65,7 +59,6 @@ export default function CatalogoCarrosCliente() {
     fetchCars();
   }, [fetchCars]);
 
-  // Função para aplicar os filtros quando o botão "Filtrar" é pressionado
   const applyFilters = () => {
     setAppliedFilters(filters);
   };
@@ -73,129 +66,36 @@ export default function CatalogoCarrosCliente() {
   return (
     <div className='dividir'>
       <div className="filters-container">
-        {/* Filtros de Marca, Modelo e Ano no topo */}
         <label>Marca</label>
-        <input
-          type="text"
-          name="brand"
-          value={filters.brand}
-          onChange={handleFilterChange}
-        />
+        <input type="text" name="brand" value={filters.brand} onChange={handleFilterChange} />
 
         <label>Modelo</label>
-        <input
-          type="text"
-          name="model"
-          value={filters.model}
-          onChange={handleFilterChange}
-        />
+        <input type="text" name="model" value={filters.model} onChange={handleFilterChange} />
 
         <label>Ano</label>
-        <input
-          type="text"
-          name="year"
-          value={filters.year}
-          onChange={handleFilterChange}
-        />
+        <input type="text" name="year" value={filters.year} onChange={handleFilterChange} />
 
-        {/* Checkboxes para Disponibilidade */}
         <label className='check-aluguel'>
-          <input className='checkbox'
-            type="checkbox"
-            name="isForSale"
-            checked={filters.isForSale}
-            onChange={handleFilterChange}
-          />
+          <input className='checkbox' type="checkbox" name="isForSale" checked={filters.isForSale} onChange={handleFilterChange} />
           Disponível para Venda
         </label>
-
-        {/* Faixa de Preço para Venda - exibida apenas se "Disponível para Venda" estiver marcado */}
-        {filters.isForSale && (
-          <>
-            <label className='range'>
-              <p>Faixa de Preço para Venda:</p>
-              <p>R$ {Number(filters.minSalePrice).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} - R$ {Number(filters.maxSalePrice).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-            </label>
-            
-            <input
-              type="range"
-              name="minSalePrice"
-              min="5000"
-              max="100000"
-              value={filters.minSalePrice}
-              onChange={handleFilterChange}
-            />
-            <input
-              type="range"
-              name="maxSalePrice"
-              min="5000"
-              max="100000"
-              value={filters.maxSalePrice}
-              onChange={handleFilterChange}
-            />
-          </>
-        )}
-
         <label>
-          <input
-            className='checkbox'
-            type="checkbox"
-            name="isForRent"
-            checked={filters.isForRent}
-            onChange={handleFilterChange}
-          />
+          <input className='checkbox' type="checkbox" name="isForRent" checked={filters.isForRent} onChange={handleFilterChange} />
           Disponível para Aluguel
         </label>
   
-        {/* Faixa de Preço para Aluguel - exibida apenas se "Disponível para Aluguel" estiver marcado */}
-        {filters.isForRent && (
-          <>
-            <label className='range'>
-              <p>Faixa de Preço para Aluguel:</p>
-              <p>R$ {Number(filters.minRentPrice).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} - R$ {Number(filters.maxRentPrice).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-            </label>
-            <input
-              type="range"
-              name="minRentPrice"
-              min="0"
-              max="5000"
-              value={filters.minRentPrice}
-              onChange={handleFilterChange}
-            />
-            <input
-              type="range"
-              name="maxRentPrice"
-              min="0"
-              max="5000"
-              value={filters.maxRentPrice}
-              onChange={handleFilterChange}
-            />
-          </>
-        )}
+        <label className='range'><p>Faixa de Preço para Aluguel:</p><p>{filters.minRentPrice} - {filters.maxRentPrice}</p></label>
+        <input type="range" name="minRentPrice" min="0" max="5000" value={filters.minRentPrice} onChange={handleFilterChange} disabled={!filters.isForRent} />
+        <input type="range" name="maxRentPrice" min="0" max="5000" value={filters.maxRentPrice} onChange={handleFilterChange} disabled={!filters.isForRent} />
   
-        {/* Faixa de Quilometragem */}
-        <label className='rang'>
-          <p>Faixa de Quilometragem:</p> 
-          <p>{Number(filters.minMileage).toLocaleString('pt-BR')} km - {Number(filters.maxMileage).toLocaleString('pt-BR')} km</p>
-        </label>
-        <input
-          type="range"
-          name="minMileage"
-          min="0"
-          max="300000"
-          value={filters.minMileage}
-          onChange={handleFilterChange}
-        />
-        <input
-          type="range"
-          name="maxMileage"
-          min="0"
-          max="300000"
-          value={filters.maxMileage}
-          onChange={handleFilterChange}
-        />
+        <label className='range'><p>Faixa de Preço para Venda:</p><p>{filters.minSalePrice} - {filters.maxSalePrice}</p></label>
+        <input type="range" name="minSalePrice" min="5000" max="100000" value={filters.minSalePrice} onChange={handleFilterChange} disabled={!filters.isForSale} />
+        <input type="range" name="maxSalePrice" min="5000" max="100000" value={filters.maxSalePrice} onChange={handleFilterChange} disabled={!filters.isForSale} />
+  
+        <label className='rang'><p>Faixa de Quilometragem:</p><p>{filters.minMileage} - {filters.maxMileage}</p></label>
+        <input type="range" name="minMileage" min="0" max="300000" value={filters.minMileage} onChange={handleFilterChange} />
+        <input type="range" name="maxMileage" min="0" max="300000" value={filters.maxMileage} onChange={handleFilterChange} />
         
-        {/* Botão para aplicar filtros */}
         <button className='filtrar-btn' onClick={applyFilters}>Filtrar</button>
       </div>
   
