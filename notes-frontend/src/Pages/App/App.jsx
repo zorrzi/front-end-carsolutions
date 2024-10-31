b10f6cfb9412afbedec6c0bbd0b17f1bd4cf94d7
Mateus Marinheiro <mateusmarinheiro2005@gmail.com>
2024-10-31 17:40:01 -0300

    carrossel responsivo

--- notes-frontend/src/Pages/App/App.jsx
+++ notes-frontend/src/Pages/App/App.jsx
@@ -6,27 +6,56 @@ import Footer from "../../components/Footer";
 import Destaques from "../../components/Destaques";
 import BotaoChat from "../../components/BotaoChat";
 import ChatCliente from "../../components/ChatCliente";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ComoFunciona from "../../components/ComoFunciona";
 import FeedbackClientes from "../../components/FeedbackClientes";
 import Carrossel from "../../components/Carrossel";
 
-
-const featuredCars = [
+const featuredCars1 = [
   {
-    image: "/1.png"
+    image: "/11.png"
   },
   {
-    image: '/2.png'
+    image: '/21.png'
   },
   {
-    image: '/3.png'
+    image: '/31.png'
   }
 ];
 
+const featuredCars2 = [
+  {
+    image: "/12.png"
+  },
+  {
+    image: '/22.png'
+  },
+  {
+    image: '/32.png'
+  }
+];
 
 function App() {
   const [isChatOpen, setIsChatOpen] = useState(false); // Define o estado para abrir/fechar o chat
+  const [featuredCars, setFeaturedCars] = useState(featuredCars1); // Estado para o carrossel de carros
+
+  // Atualiza featuredCars com base na largura da tela
+  useEffect(() => {
+    const updateFeaturedCars = () => {
+      if (window.innerWidth < 560) {
+        setFeaturedCars(featuredCars2);
+      } else {
+        setFeaturedCars(featuredCars1);
+      }
+    };
+
+    updateFeaturedCars(); // Chamada inicial para definir o valor correto
+
+    window.addEventListener("resize", updateFeaturedCars); // Listener para monitorar mudanças de tamanho
+
+    return () => window.removeEventListener("resize", updateFeaturedCars); // Limpeza do listener
+  }, []);
+
   return (
     <div>
       <Header />
@@ -42,15 +71,12 @@ function App() {
       <FeedbackClientes />
       
       <div>
-      <BotaoChat setIsChatOpen={setIsChatOpen} /> {/* Passa setIsChatOpen como prop */}
-      <ChatCliente isChatOpen={isChatOpen} setIsChatOpen={setIsChatOpen} /> {/* Passa isChatOpen e setIsChatOpen */}
+        <BotaoChat setIsChatOpen={setIsChatOpen} /> {/* Passa setIsChatOpen como prop */}
+        <ChatCliente isChatOpen={isChatOpen} setIsChatOpen={setIsChatOpen} /> {/* Passa isChatOpen e setIsChatOpen */}
       </div>
 
       <Footer />
-
     </div>
-    
-
   );
 }
 
