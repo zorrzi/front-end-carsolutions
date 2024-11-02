8020458c3c389d177889cad182ed92371b9810a7
zorrzi <eduardovz@al.insper.edu.br>
2024-11-02 01:10:56 -0300

    funcionalidade de retirar o desconto de um carro

--- notes-frontend/src/components/CarroFuncionario/index.jsx
+++ notes-frontend/src/components/CarroFuncionario/index.jsx
@@ -1,8 +1,10 @@
+// components/CarroFuncionario.jsx
+
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './index.css';
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL; 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export default function CarroFuncionario({ car, loadCars, isSelected, toggleSelection }) {
   const deleteCar = (event) => {
@@ -15,6 +17,22 @@ export default function CarroFuncionario({ car, loadCars, isSelected, toggleSele
       })
       .catch((err) => {
         console.error("Erro ao deletar o carro:", err);
+      });
+  };
+
+  const removeDiscount = () => {
+    axios
+      .patch(`${apiBaseUrl}/cars/remove-discount/${car.id}/`, {
+        is_discounted_sale: false,
+        discount_percentage_sale: null,
+        is_discounted_rent: false,
+        discount_percentage_rent: null
+      })
+      .then(() => {
+        loadCars(); // Recarrega os carros após a remoção do desconto
+      })
+      .catch((err) => {
+        console.error("Erro ao remover o desconto:", err);
       });
   };
 
@@ -32,8 +50,16 @@ export default function CarroFuncionario({ car, loadCars, isSelected, toggleSele
         <p className="card-title">{car.brand}</p>
         <p className="card-subtitle">{car.model}</p>
       </div>
-      <div>
+      <div className="image-container">
         <img className="carro" src={car.image_url_1 || '/default-image.jpg'} alt={car.model} />
+        
+        {/* Ícone de desconto com botão "X" para remover */}
+        {(car.is_discounted_sale || car.is_discounted_rent) && (
+          <div className="discount-icon-container">
+            <img src="desconto.png" alt="Desconto" className="discount-icon" />
+            <button className="remove-discount-button" onClick={removeDiscount}>X</button>
+          </div>
+        )}
       </div>
 
       {/* Seção de banner para Aluguel e Venda */}
