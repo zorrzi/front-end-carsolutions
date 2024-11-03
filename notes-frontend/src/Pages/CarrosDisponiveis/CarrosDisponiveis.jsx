cfacfb645dcb9945f4994e8467cbb61dce0bb628
Mateus Marinheiro <mateusmarinheiro2005@gmail.com>
2024-11-03 17:22:41 -0300

    ajustando estilo da agenda do cliente

--- notes-frontend/src/Pages/CarrosDisponiveis/CarrosDisponiveis.jsx
+++ notes-frontend/src/Pages/CarrosDisponiveis/CarrosDisponiveis.jsx
@@ -5,12 +5,14 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import FundoIncial from "../../components/FundoInicial";
 import CatalogoCarrosCliente from "../../components/CatalogoCarrosCliente";
+import BotaoVoltar from "../../components/BotaoVoltar";
 
 function CarrosDisponiveis() {
   return (
     <>
     <Header />
     <FundoIncial />
+    <BotaoVoltar />
     <h1 className="destaques">Carros Disponíveis</h1>
     <div className="formulario-aluguel">
     <CatalogoCarrosCliente />
