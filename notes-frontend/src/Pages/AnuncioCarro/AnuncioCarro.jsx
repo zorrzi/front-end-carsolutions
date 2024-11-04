dea25fbc57990af0ab06abbd89c2e73b3a63f752
zorrzi <eduardovz@al.insper.edu.br>
2024-11-04 14:41:14 -0300

    carros semelhantes

--- notes-frontend/src/Pages/AnuncioCarro/AnuncioCarro.jsx
+++ notes-frontend/src/Pages/AnuncioCarro/AnuncioCarro.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './AnuncioCarro.css';
 import InformacoesCarro from '../../components/InformacoesCarro';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import FichaTecnica from '../../components/FichaTecnica';
 import BotaoVoltar from '../../components/BotaoVoltar';
-
+import CarrosSimilares from '../../components/CarrosSimilares';
+import { useParams } from 'react-router-dom';
 
 function AnuncioCarro() {
+  const { id } = useParams();
+  const carrosSimilaresRef = useRef(null);
+
+  const handleScrollToSimilares = () => {
+    carrosSimilaresRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <>
-    
-    <Header />
-    
-    <BotaoVoltar />
+      <Header />
+      <BotaoVoltar />
+      <button className="botao-similares" onClick={handleScrollToSimilares}>
+        Anúncios Similares
+      </button>
+
+      <InformacoesCarro />
+      
 
 
-    <InformacoesCarro />
-    
-    <FichaTecnica />
+      <FichaTecnica />
 
-    <Footer />
+      <div ref={carrosSimilaresRef}>
+        <CarrosSimilares carId={id} />
+      </div>
 
-    
+      <Footer />
     </>
-    
-
   );
 }
 
